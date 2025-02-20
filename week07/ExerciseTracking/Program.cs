7efd10b3006a6b030543de0a230285f500f508f5
Nkechiyere <nwa22011@byui.edu>
2025-02-20 07:27:02 +0100

    week7

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        List<BaseActivity> activities = new List<BaseActivity>
+        {
+            new RunningActivity(DateTime.Now.AddDays(-1), 30, 5.0),
+            new CyclingActivity(DateTime.Now.AddDays(-2), 45, 20.0),
+            new SwimmingActivity(DateTime.Now.AddDays(-3), 60, 30)
+        };
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+            Console.WriteLine();
+        }
     }
 }
